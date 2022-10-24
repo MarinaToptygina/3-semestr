b378d401a1d109a862bb49fd242a1bf1e98b155b
MarinaToptygina <91845711+MarinaToptygina@users.noreply.github.com>
2022-10-24 11:33:58 +0300

    Add files via upload

---
+++ dz1.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Text.RegularExpressions;
+string str1;
+str1 = Console.ReadLine();
+Console.WriteLine("first word-" + str1.ToLower());
+
+
+string str2;
+str2 = Console.ReadLine();
+Console.WriteLine("second word-" + str2.ToLower());
+if (str1.Length != str2.Length)
+{
+    Console.WriteLine("error");
+}
+
+string res = "";
+for (int i = 0; i < str1.Length; i++)
+{
+    for (int j = 0; j < str2.Length; j++)
+    {
+        if (str1[i] == str2[j])
+        {
+            res += str2[j];
+            str2 = str2.Remove(j, 1);
+        }
+    }
+}
+if (res.Length == str1.Length)
+    Console.WriteLine("Yes");
+else Console.WriteLine("No");
+
